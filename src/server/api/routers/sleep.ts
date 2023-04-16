19ff1f9adc018158844cec162405f11902f0299d
user <user@localhost>
2023-04-16 16:18:54 +0200

    Add sleep page and router, delete example router

---
+++ src/server/api/routers/sleep.ts
@@ -0,0 +1,33 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
+
+export const sleepRouter = createTRPCRouter({
+    getAll: publicProcedure
+    .query(({ctx}) => {
+        return ctx.prisma.sleep.findMany();
+    }),
+    upsert: publicProcedure
+    .input(
+        z.object({
+          date: z.date(),
+          wakeUpTime: z.date().optional(),
+          wakeUpTired: z.boolean().optional(),
+          getUpTime: z.date().optional(),
+          bedTime: z.date().optional(),
+          sleepTime: z.date().optional(),
+        }),
+    )
+    .mutation(({ctx, input}) => {
+      console.log("tRPC server sleepRouter.upsert:")
+      console.log(input)
+
+      return ctx.prisma.sleep.upsert({
+        where: {
+          date: input.date
+        },
+        update: input,
+        create: input,
+      })
+    }),
+});
